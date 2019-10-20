747977d0c1f1df0470b79f494533c81d5aea96be
VladimirRed <badmantown42@gmail.com>
2019-10-20 19:05:20 +0300

    connected to MySQL

---
+++ WebApplication/src/main/java/by/epam/training/database/DbMain.java
@@ -0,0 +1,48 @@
+package by.epam.training.database;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class DbMain {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String url = "jdbc:mysql://localhost:3306/project_database";
+        Properties properties = new Properties();
+        properties.put("user", "root");
+        properties.put("password", "1256");
+        properties.put("autoReconnect", "true");
+        properties.put("characterEncoding", "UTF-8");
+        properties.put("useUnicode", "true");
+        properties.put("useSSL", "false");
+        try (Connection connection = DriverManager.getConnection(url, properties);
+             Statement statement = connection.createStatement()) {
+
+            String sql = "SELECT id, lastName, phone FROM users";
+            ResultSet resultSet = statement.executeQuery(sql);
+            List<Abonent> abonents = new ArrayList<>();
+
+//            resultSet.moveToInsertRow();
+//            resultSet.updateInt(1, 3);
+//            resultSet.updateString(2, "Meleshkevich");
+//            resultSet.updateInt(3, 256000502);
+//            resultSet.insertRow();
+//            resultSet.moveToCurrentRow();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                int number = resultSet.getInt(3);
+                abonents.add(new Abonent(id, name, number));
+            }
+            System.out.println(abonents);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}

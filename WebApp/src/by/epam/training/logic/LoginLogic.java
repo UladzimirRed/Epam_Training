251368497168aaf896ecaca578a5336cff695167
VladimirRed <badmantown42@gmail.com>
2019-11-05 17:54:51 +0300

    unnecessary files deleted

--- WebApp/src/by/epam/training/logic/LoginLogic.java
+++
@@ -1,9 +0,0 @@
-package by.epam.training.logic;
-
-public class LoginLogic {
-    private final static String ADMIN_LOGIN = "admin";
-    private final static String ADMIN_PASS = "Qwe123";
-    public static boolean checkLogin(String enterLogin, String enterPassword){
-        return ADMIN_LOGIN.equals(enterLogin) && ADMIN_PASS.equals(enterPassword);
-    }
-}
